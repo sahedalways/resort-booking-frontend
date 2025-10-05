6bb86ed992be9e9ce34e73a0fda3d0582431394a
mahmud4020 <72188853+mahmud4020@users.noreply.github.com>
2025-10-05 15:02:04 +0600

    event details ui update

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,4 +1,28 @@
+"use client";
 import Image from "next/image";
+import React from "react";
+import LightGallery from "lightgallery/react";
+import Link from "next/link";
+
+// import styles
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+
+// import plugins if you need
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+
+const images = [
+  { src: "/img/event-2.jpg", alt: "Event 1" },
+  { src: "/img/event-3.jpg", alt: "Engagement" },
+  { src: "/img/event-5.jpg", alt: "Corporate" },
+  { src: "/img/event-6.jpg", alt: "Corporate" },
+  { src: "/img/event-2.jpg", alt: "Event 1" },
+  { src: "/img/event-3.jpg", alt: "Engagement" },
+  { src: "/img/event-5.jpg", alt: "Corporate" },
+  { src: "/img/event-6.jpg", alt: "Corporate" },
+];
 
 export default function EventDetails() {
   return (
@@ -79,20 +103,167 @@ export default function EventDetails() {
         </div>
       </section>
 
-      <section className="section-gap">
-        <div className="container">
-          <div className="row g-4">
-            <div className="col">
-              <div className="text-center">
-                <h3 className="text-block-32 position-relative primary-color">Flawless Wedding Planning for Your Dream Day</h3>
-                <p className="subtitle text-dark">
-                  We provide exceptional service to make your vacation perfect
-                </p>
-              </div>
+    <section className="section-gap">
+  <div className="container">
+    <div className="row g-4">
+      <div className="col">
+        <div className="text-center">
+          <h3 className="text-block-32 position-relative primary-color">
+            Flawless Wedding Planning for Your Dream Day
+          </h3>
+          <p className="subtitle text-dark">
+            We provide exceptional service to make your vacation perfect
+          </p>
+        </div>
+      </div>
+
+      {/* ✅ Replace inner row with a column */}
+      <div className="col-12">
+        <LightGallery
+          speed={500}
+          plugins={[lgThumbnail, lgZoom]}
+          elementClassNames="row g-4"
+        >
+          {images.map((image, idx) => (
+            <a href={image.src} className="col-md-3" key={idx}>
+              <Image
+                src={image.src}
+                alt={image.alt}
+                width={500}
+                height={500}
+                className="img-fluid"
+              />
+            </a>
+          ))}
+        </LightGallery>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+	    <section className="section-gap">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="section-header-container">
+              <h1 className="main-title">
+                Ready to Make It Happen? Let’s Talk!
+              </h1>
+              <p className="primary-color fw-semibold col-md-8 col-12">
+                Let us help you find the venue of your dreams. Contact us today
+                to start your journey to an unforgettable
+              </p>
             </div>
           </div>
+
+          <div className="col-md-8 mx-auto">
+            <form>
+              <div className="row mb-3">
+                <div className="col-md-6 mb-3 mb-md-0">
+                  <input
+                    type="text"
+                    className="form-control shadow-none"
+                    id="yourName"
+                    placeholder="Your Name *"
+                    required
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    type="tel"
+                    className="form-control shadow-none"
+                    id="yourPhone"
+                    placeholder="Your Phone *"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="row mb-3">
+                <div className="col-md-6 mb-3 mb-md-0">
+                  <input
+                    type="text"
+                    className="form-control shadow-none"
+                    id="dateOfFunction"
+                    placeholder="Date of Function *"
+                    required
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    type="number"
+                    className="form-control shadow-none"
+                    id="gatheringSize"
+                    placeholder="Gathering Size *"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="row mb-3">
+                <div className="col-md-6 mb-3 mb-md-0">
+                  <input
+                    type="text"
+                    className="form-control shadow-none"
+                    id="preferredLocation"
+                    placeholder="Preferred Location *"
+                    required
+                  />
+                </div>
+                <div className="col-md-6">
+                  <div className="dropdown">
+                    <button
+                      className="btn btn-outline-secondary dropdown-toggle custom-dropdown-style"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                    >
+                      Select Budget
+                    </button>
+                    <ul className="dropdown-menu w-100">
+                      <li>
+                        <Link className="dropdown-item" href="#">
+                          $100 - $500
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" href="#">
+                          $500 - $1,000
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" href="#">
+                          $1,000+
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              <div className="row mb-4">
+                <div className="col-12">
+                  <textarea
+                    className="form-control shadow-none"
+                    id="tellUsMore"
+                    rows={5}
+                    placeholder="Tell Us More"
+                  ></textarea>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-12 text-center">
+                  <button type="submit" className="btn btn-custom">
+                    SEND YOUR MESSAGE
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-      </section>
+      </div>
+    </section>
     </>
   );
 }
