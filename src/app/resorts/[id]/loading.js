eaea8788a8739f4c2db7202e17e9277fb1e97534
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2025-10-25 10:46:11 +0600

    loading modified

--- src/app/resorts/[id]/loading.js
+++ src/app/resorts/[id]/loading.js
@@ -1,16 +1,19 @@
 "use client";
 
+import Image from "next/image";
+
 export default function GlobalLoading() {
   return (
     <div className="full-page-loader d-flex flex-column align-items-center justify-content-center">
-      <img
-        src="/HMS Loading.gif"
+      <Image
+        src="/Trail loading.gif"
         alt="Loading..."
-        className="mb-3"
+        width={120}
+        height={120}
+        priority
         style={{
-          width: "280px",
-          height: "280px",
           objectFit: "contain",
+          imageRendering: "high-quality",
         }}
       />
     </div>
