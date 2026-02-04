4eb24534735a5eec290de7c1da93817734d50119
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2026-02-04 09:55:16 +0600

    config changed for showcase

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "admin.bookingxpert.org",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "buzzhiring.shopsatkhira.com",
-        pathname: "/**",
-      },
-    ],
-    formats: ["image/avif", "image/webp"],
-    minimumCacheTTL: 60,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+    trailingSlash: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: '127.0.0.1',
+                port: '8000',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'buzzhiring.shopsatkhira.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'buzzhiring.shopsatkhira.com',
+                pathname: '/**',
+            },
+        ],
+        formats: ['image/avif', 'image/webp'],
+        minimumCacheTTL: 60,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
